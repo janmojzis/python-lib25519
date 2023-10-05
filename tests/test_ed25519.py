9c2a9898fb1f5d0d3e616dca3634f8e4b627ae61
Jan Mojžíš <jan.mojzis@gmail.com>
2023-10-05 10:04:35 +0200

    tests/test_ed25519.py move imports from test function

--- tests/test_ed25519.py
+++ tests/test_ed25519.py
@@ -1,8 +1,7 @@
+from lib25519 import ed25519
+import os
+
 def test_ed25519():
-
-    from lib25519 import ed25519
-    import os
-
     pk, sk = ed25519.keypair()
     m1 = os.urandom(128)
     sm = ed25519.sign(m1, sk)
